b66c9a6da4bce6a17cce6efe849ecda783bf25ad
Emerson Carlos <emcchost@gmail.com>
2020-05-05 17:36:21 -0300

    Leitura dos dados

---
+++ src/aplicaçao/Programa.java
@@ -0,0 +1,31 @@
+package aplicaçao;
+
+import java.util.Locale ;
+import java.util.Scanner;
+
+public class Programa {
+
+	public static void main(String[] args) {
+		
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		
+		char sexo;
+		int cervejas, refrigerantes, espetos;
+		
+		System.out.print("Sexo: ");
+		sexo = sc.next().charAt(0);
+		System.out.print("Quantidade de cervejas: ");
+		cervejas = sc.nextInt();
+		System.out.print("Quantidade de refrigerantes: ");
+		refrigerantes  = sc.nextInt();
+		System.out.print("Quantidade de espetinhos: ");
+		espetos  = sc.nextInt();
+		
+		
+		sc.close();
+
+	}
+
+}
